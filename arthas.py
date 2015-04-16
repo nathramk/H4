82e587ae0eff2b005cabc97f595bbdd1562b9073
nathramk <nathramk@gmail.com>
2015-04-16 16:56:45 -0500

    version2

--- arthas.py
+++ arthas.py
@@ -1,3 +1,13 @@
-print "hola mundo"
-
-print "==="
+#programa que diga si eres oven, adulto o abuelo
+#entrada
+edad=input("ingrese su edad: ")
+#proceso
+if edad<18 and edad<=18:
+    r="usted es joven"
+else:
+    if edad>18 and edad<=60:
+        r="usted es adulto"
+    else:
+        r="usted es mayor de edad"
+#salida
+print r
